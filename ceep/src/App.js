9f45a4764d61657dd0860baf99189adb272c13d3
Guilherme Lima <glima@noovi.com.br>
2022-02-14 16:10:00 -0300

    Trabalhando com Binds, Props e States

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,11 +5,29 @@ import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
-  render() {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      notas:[]
+    };
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota];
+    const novoEstado = {
+      notas:novoArrayNotas
+    }    
+    this.setState(novoEstado);   
+  }
+
+  render() {    
     return (
       <section className="conteudo">
-        <Formulario/>
-        <ListaDeNotas/>
+        <Formulario criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import CardNotas from "../CardNota";
-import "./estilo.css"
+import "./estilo.css";
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-      {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
-        return (
-          <li className="lista-notas_item" key={index}>
-            <CardNotas />
-          </li>
-        );
-      })}
-    </ul>
+        {/*isso faz com que carregue um array de notas */}
+        {this.props.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNotas titulo={[nota.titulo]} texto={[nota.texto]} />
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
